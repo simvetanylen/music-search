d8f3f2f2a66b07569e7307c9c69b84588e5fd3ab
Oreste Viron <oreste.viron@gmail.com>
2024-05-06 08:09:35 +0200

    Add custom analysis

--- src/main/java/com/music/album/AlbumDocument.java
+++ src/main/java/com/music/album/AlbumDocument.java
@@ -2,22 +2,20 @@ package com.music.album;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document(indexName = "albums")
+@Setting(settingPath = "/analyzer/custom-analyzer.json")
 @Data
 public class AlbumDocument {
     @Id
     private UUID id;
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, analyzer = "custom_analyzer")
     private String title;
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, analyzer = "custom_analyzer")
     private String artist;
     @Field(type = FieldType.Integer)
     private Integer releaseYear;
